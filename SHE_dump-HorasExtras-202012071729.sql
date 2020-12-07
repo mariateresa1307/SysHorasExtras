020f950a4cde14197573613f2d298e33bdf2e890
a05j96@gmail.com <a05j96@gmail.com>
2020-12-07 17:31:10 -0400

    fix

--- SHE_dump-HorasExtras-202012071729.sql
+++ SHE_dump-HorasExtras-202012071729.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 10.9
 -- Dumped by pg_dump version 10.9
 
--- Started on 2020-11-28 22:56:05 -04
+-- Started on 2020-12-07 17:29:43 -04
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -50,7 +50,7 @@ CREATE SEQUENCE public.cargo_id_seq
 
 
 --
--- TOC entry 2358 (class 0 OID 0)
+-- TOC entry 2370 (class 0 OID 0)
 -- Dependencies: 214
 -- Name: cargo_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
@@ -69,7 +69,8 @@ CREATE TABLE public.control_asistencia (
     salida timestamp without time zone,
     funcionario_id integer,
     tiempo_extra character varying,
-    tiempo_atraso character varying
+    tiempo_atraso character varying,
+    registro_asistencia_mensual_id integer
 );
 
 
@@ -99,7 +100,7 @@ CREATE SEQUENCE public.departamento_id_seq
 
 
 --
--- TOC entry 2359 (class 0 OID 0)
+-- TOC entry 2371 (class 0 OID 0)
 -- Dependencies: 212
 -- Name: departamento_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
@@ -141,7 +142,7 @@ CREATE SEQUENCE public.funcionario_id_seq
 
 
 --
--- TOC entry 2360 (class 0 OID 0)
+-- TOC entry 2372 (class 0 OID 0)
 -- Dependencies: 210
 -- Name: funcionario_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
@@ -175,7 +176,7 @@ CREATE SEQUENCE public.miscelaneas_id_seq
 
 
 --
--- TOC entry 2361 (class 0 OID 0)
+-- TOC entry 2373 (class 0 OID 0)
 -- Dependencies: 218
 -- Name: miscelaneas_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
@@ -198,12 +199,48 @@ CREATE SEQUENCE public.recontrol_asistencia_id_seq
 
 
 --
--- TOC entry 2362 (class 0 OID 0)
+-- TOC entry 2374 (class 0 OID 0)
 -- Dependencies: 216
 -- Name: recontrol_asistencia_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE public.recontrol_asistencia_id_seq OWNED BY public.control_asistencia.id;
+
+
+--
+-- TOC entry 225 (class 1259 OID 33019)
+-- Name: registro_asistencia_mensual; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.registro_asistencia_mensual (
+    id integer NOT NULL,
+    aprobado_coordinador boolean,
+    aprobado_rrhh boolean,
+    tiempo_ date
+);
+
+
+--
+-- TOC entry 224 (class 1259 OID 33017)
+-- Name: registro_asistencia_mensual_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.registro_asistencia_mensual_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- TOC entry 2375 (class 0 OID 0)
+-- Dependencies: 224
+-- Name: registro_asistencia_mensual_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.registro_asistencia_mensual_id_seq OWNED BY public.registro_asistencia_mensual.id;
 
 
 --
@@ -252,7 +289,7 @@ CREATE SEQUENCE public.usuario_id_seq
 
 
 --
--- TOC entry 2363 (class 0 OID 0)
+-- TOC entry 2376 (class 0 OID 0)
 -- Dependencies: 220
 -- Name: usuario_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
@@ -286,7 +323,7 @@ CREATE SEQUENCE public.usuario_tipo_id_seq
 
 
 --
--- TOC entry 2364 (class 0 OID 0)
+-- TOC entry 2377 (class 0 OID 0)
 -- Dependencies: 222
 -- Name: usuario_tipo_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
@@ -295,7 +332,7 @@ ALTER SEQUENCE public.usuario_tipo_id_seq OWNED BY public.usuario_tipo.id;
 
 
 --
--- TOC entry 2192 (class 2604 OID 32942)
+-- TOC entry 2198 (class 2604 OID 32942)
 -- Name: cargo id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -303,7 +340,7 @@ ALTER TABLE ONLY public.cargo ALTER COLUMN id SET DEFAULT nextval('public.cargo_
 
 
 --
--- TOC entry 2193 (class 2604 OID 32958)
+-- TOC entry 2199 (class 2604 OID 32958)
 -- Name: control_asistencia id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -311,7 +348,7 @@ ALTER TABLE ONLY public.control_asistencia ALTER COLUMN id SET DEFAULT nextval('
 
 
 --
--- TOC entry 2191 (class 2604 OID 32917)
+-- TOC entry 2197 (class 2604 OID 32917)
 -- Name: departamento id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -319,7 +356,7 @@ ALTER TABLE ONLY public.departamento ALTER COLUMN id SET DEFAULT nextval('public
 
 
 --
--- TOC entry 2190 (class 2604 OID 32907)
+-- TOC entry 2196 (class 2604 OID 32907)
 -- Name: funcionario id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -327,7 +364,7 @@ ALTER TABLE ONLY public.funcionario ALTER COLUMN id SET DEFAULT nextval('public.
 
 
 --
--- TOC entry 2194 (class 2604 OID 32971)
+-- TOC entry 2200 (class 2604 OID 32971)
 -- Name: miscelaneas id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -335,7 +372,15 @@ ALTER TABLE ONLY public.miscelaneas ALTER COLUMN id SET DEFAULT nextval('public.
 
 
 --
--- TOC entry 2195 (class 2604 OID 32982)
+-- TOC entry 2203 (class 2604 OID 33038)
+-- Name: registro_asistencia_mensual id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.registro_asistencia_mensual ALTER COLUMN id SET DEFAULT nextval('public.registro_asistencia_mensual_id_seq'::regclass);
+
+
+--
+-- TOC entry 2201 (class 2604 OID 32982)
 -- Name: usuario id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -343,7 +388,7 @@ ALTER TABLE ONLY public.usuario ALTER COLUMN id SET DEFAULT nextval('public.usua
 
 
 --
--- TOC entry 2196 (class 2604 OID 32993)
+-- TOC entry 2202 (class 2604 OID 32993)
 -- Name: usuario_tipo id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -351,43 +396,53 @@ ALTER TABLE ONLY public.usuario_tipo ALTER COLUMN id SET DEFAULT nextval('public
 
 
 --
--- TOC entry 2343 (class 0 OID 32939)
+-- TOC entry 2353 (class 0 OID 32939)
 -- Dependencies: 215
 -- Data for Name: cargo; Type: TABLE DATA; Schema: public; Owner: -
 --
 
 INSERT INTO public.cargo VALUES (1, 'programador', 1200000, 1);
+INSERT INTO public.cargo VALUES (2, 'sws', 22, 1);
+INSERT INTO public.cargo VALUES (3, 'inf', 323, 1);
 
 
 --
--- TOC entry 2345 (class 0 OID 32955)
+-- TOC entry 2355 (class 0 OID 32955)
 -- Dependencies: 217
 -- Data for Name: control_asistencia; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.control_asistencia VALUES (43, '2020-11-28 15:11:20', '2020-11-28 15:12:18', 1, '1:12', '0:41');
+INSERT INTO public.control_asistencia VALUES (46, '2020-12-07 17:12:21', NULL, 1, NULL, '2:42', 6);
 
 
 --
--- TOC entry 2341 (class 0 OID 32914)
+-- TOC entry 2351 (class 0 OID 32914)
 -- Dependencies: 213
 -- Data for Name: departamento; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.departamento VALUES (1, 'informatica');
+INSERT INTO public.departamento VALUES (2, 'proyecto');
+INSERT INTO public.departamento VALUES (1, 'informaticassxx');
+INSERT INTO public.departamento VALUES (3, '   sws  sw sxsxs');
+INSERT INTO public.departamento VALUES (4, 'xs');
+INSERT INTO public.departamento VALUES (5, 'swsw');
 
 
 --
--- TOC entry 2339 (class 0 OID 32904)
+-- TOC entry 2349 (class 0 OID 32904)
 -- Dependencies: 211
 -- Data for Name: funcionario; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.funcionario VALUES (1, 2333, 'alei', 'df', 'hr', NULL, NULL, NULL, true, 1);
+INSERT INTO public.funcionario VALUES (2, 1, '2', '', '4', '5', 'opcion-1', 6, NULL, NULL);
+INSERT INTO public.funcionario VALUES (3, 1, '2', '', '4', '5', '1', 6, NULL, NULL);
+INSERT INTO public.funcionario VALUES (4, 12, '2', '3', '4', '5', '7', 6, false, 1);
+INSERT INTO public.funcionario VALUES (5, 1, '1', '1', '1', '1', '1', 1, NULL, 1);
+INSERT INTO public.funcionario VALUES (1, 123, 'alei', 'df', 'hr', '1', '2', 23, false, 1);
 
 
 --
--- TOC entry 2347 (class 0 OID 32968)
+-- TOC entry 2357 (class 0 OID 32968)
 -- Dependencies: 219
 -- Data for Name: miscelaneas; Type: TABLE DATA; Schema: public; Owner: -
 --
@@ -399,7 +454,20 @@ INSERT INTO public.miscelaneas VALUES (1, '{
 
 
 --
--- TOC entry 2349 (class 0 OID 32979)
+-- TOC entry 2363 (class 0 OID 33019)
+-- Dependencies: 225
+-- Data for Name: registro_asistencia_mensual; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public.registro_asistencia_mensual VALUES (2, true, NULL, '2020-01-01');
+INSERT INTO public.registro_asistencia_mensual VALUES (3, true, NULL, '2020-01-02');
+INSERT INTO public.registro_asistencia_mensual VALUES (4, true, NULL, '2020-02-01');
+INSERT INTO public.registro_asistencia_mensual VALUES (5, true, NULL, '2020-03-01');
+INSERT INTO public.registro_asistencia_mensual VALUES (6, false, false, '2020-12-07');
+
+
+--
+-- TOC entry 2359 (class 0 OID 32979)
 -- Dependencies: 221
 -- Data for Name: usuario; Type: TABLE DATA; Schema: public; Owner: -
 --
@@ -409,46 +477,51 @@ INSERT INTO public.usuario VALUES (3, '1ss', '2', 358, false, '', 1, 1, false);
 INSERT INTO public.usuario VALUES (1, '1sss', '2', 3, false, '', 1, 1, true);
 INSERT INTO public.usuario VALUES (5, 'sw', 'ws', 12, false, '2', 1, 1, NULL);
 INSERT INTO public.usuario VALUES (6, 'ded', 'dede MOD', 323232323, false, '', 1, 1, true);
+INSERT INTO public.usuario VALUES (7, 'aleisei', '22', 222, false, '', 1, 1, true);
+INSERT INTO public.usuario VALUES (8, '1w2', 'w2w', 222, false, '222', 1, 2, NULL);
+INSERT INTO public.usuario VALUES (9, '1', '2f', 3, false, '', 1, 1, true);
+INSERT INTO public.usuario VALUES (10, '1', '1', 1, false, '1', 1, 2, NULL);
 
 
 --
--- TOC entry 2351 (class 0 OID 32990)
+-- TOC entry 2361 (class 0 OID 32990)
 -- Dependencies: 223
 -- Data for Name: usuario_tipo; Type: TABLE DATA; Schema: public; Owner: -
 --
 
 INSERT INTO public.usuario_tipo VALUES (1, 'admin');
+INSERT INTO public.usuario_tipo VALUES (2, 'asistente');
 
 
 --
--- TOC entry 2365 (class 0 OID 0)
+-- TOC entry 2378 (class 0 OID 0)
 -- Dependencies: 214
 -- Name: cargo_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.cargo_id_seq', 1, true);
+SELECT pg_catalog.setval('public.cargo_id_seq', 3, true);
 
 
 --
--- TOC entry 2366 (class 0 OID 0)
+-- TOC entry 2379 (class 0 OID 0)
 -- Dependencies: 212
 -- Name: departamento_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.departamento_id_seq', 1, true);
+SELECT pg_catalog.setval('public.departamento_id_seq', 5, true);
 
 
 --
--- TOC entry 2367 (class 0 OID 0)
+-- TOC entry 2380 (class 0 OID 0)
 -- Dependencies: 210
 -- Name: funcionario_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.funcionario_id_seq', 1, true);
+SELECT pg_catalog.setval('public.funcionario_id_seq', 5, true);
 
 
 --
--- TOC entry 2368 (class 0 OID 0)
+-- TOC entry 2381 (class 0 OID 0)
 -- Dependencies: 218
 -- Name: miscelaneas_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
@@ -457,16 +530,25 @@ SELECT pg_catalog.setval('public.miscelaneas_id_seq', 1, true);
 
 
 --
--- TOC entry 2369 (class 0 OID 0)
+-- TOC entry 2382 (class 0 OID 0)
 -- Dependencies: 216
 -- Name: recontrol_asistencia_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.recontrol_asistencia_id_seq', 43, true);
+SELECT pg_catalog.setval('public.recontrol_asistencia_id_seq', 46, true);
 
 
 --
--- TOC entry 2370 (class 0 OID 0)
+-- TOC entry 2383 (class 0 OID 0)
+-- Dependencies: 224
+-- Name: registro_asistencia_mensual_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.registro_asistencia_mensual_id_seq', 6, true);
+
+
+--
+-- TOC entry 2384 (class 0 OID 0)
 -- Dependencies: 197
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
@@ -475,25 +557,25 @@ SELECT pg_catalog.setval('public.users_id_seq', 1, false);
 
 
 --
--- TOC entry 2371 (class 0 OID 0)
+-- TOC entry 2385 (class 0 OID 0)
 -- Dependencies: 220
 -- Name: usuario_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.usuario_id_seq', 6, true);
+SELECT pg_catalog.setval('public.usuario_id_seq', 10, true);
 
 
 --
--- TOC entry 2372 (class 0 OID 0)
+-- TOC entry 2386 (class 0 OID 0)
 -- Dependencies: 222
 -- Name: usuario_tipo_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.usuario_tipo_id_seq', 1, true);
+SELECT pg_catalog.setval('public.usuario_tipo_id_seq', 2, true);
 
 
 --
--- TOC entry 2202 (class 2606 OID 32947)
+-- TOC entry 2209 (class 2606 OID 32947)
 -- Name: cargo cargo_pk; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -502,7 +584,7 @@ ALTER TABLE ONLY public.cargo
 
 
 --
--- TOC entry 2200 (class 2606 OID 32924)
+-- TOC entry 2207 (class 2606 OID 32924)
 -- Name: departamento departamento_pk; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -511,7 +593,7 @@ ALTER TABLE ONLY public.departamento
 
 
 --
--- TOC entry 2198 (class 2606 OID 32922)
+-- TOC entry 2205 (class 2606 OID 32922)
 -- Name: funcionario funcionario_pk; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -520,7 +602,7 @@ ALTER TABLE ONLY public.funcionario
 
 
 --
--- TOC entry 2206 (class 2606 OID 32976)
+-- TOC entry 2213 (class 2606 OID 32976)
 -- Name: miscelaneas miscelaneas_pk; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -529,7 +611,7 @@ ALTER TABLE ONLY public.miscelaneas
 
 
 --
--- TOC entry 2204 (class 2606 OID 32960)
+-- TOC entry 2211 (class 2606 OID 32960)
 -- Name: control_asistencia recontrol_asistencia_pk; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -538,7 +620,16 @@ ALTER TABLE ONLY public.control_asistencia
 
 
 --
--- TOC entry 2208 (class 2606 OID 32987)
+-- TOC entry 2219 (class 2606 OID 33040)
+-- Name: registro_asistencia_mensual registro_asistencia_mensual_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.registro_asistencia_mensual
+    ADD CONSTRAINT registro_asistencia_mensual_pk PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2215 (class 2606 OID 32987)
 -- Name: usuario usuario_pk; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -547,7 +638,7 @@ ALTER TABLE ONLY public.usuario
 
 
 --
--- TOC entry 2210 (class 2606 OID 32998)
+-- TOC entry 2217 (class 2606 OID 32998)
 -- Name: usuario_tipo usuario_tipo_pk; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -556,7 +647,7 @@ ALTER TABLE ONLY public.usuario_tipo
 
 
 --
--- TOC entry 2212 (class 2606 OID 33004)
+-- TOC entry 2221 (class 2606 OID 33004)
 -- Name: cargo cargo_fk; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -565,7 +656,16 @@ ALTER TABLE ONLY public.cargo
 
 
 --
--- TOC entry 2211 (class 2606 OID 32948)
+-- TOC entry 2223 (class 2606 OID 33041)
+-- Name: control_asistencia control_asistencia_fk; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.control_asistencia
+    ADD CONSTRAINT control_asistencia_fk FOREIGN KEY (registro_asistencia_mensual_id) REFERENCES public.registro_asistencia_mensual(id);
+
+
+--
+-- TOC entry 2220 (class 2606 OID 32948)
 -- Name: funcionario funcionario_cargo_fk; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -574,7 +674,7 @@ ALTER TABLE ONLY public.funcionario
 
 
 --
--- TOC entry 2213 (class 2606 OID 32961)
+-- TOC entry 2222 (class 2606 OID 32961)
 -- Name: control_asistencia recontrol_asistencia_fk; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -583,7 +683,7 @@ ALTER TABLE ONLY public.control_asistencia
 
 
 --
--- TOC entry 2214 (class 2606 OID 32999)
+-- TOC entry 2224 (class 2606 OID 32999)
 -- Name: usuario usuario_fk; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -592,7 +692,7 @@ ALTER TABLE ONLY public.usuario
 
 
 --
--- TOC entry 2215 (class 2606 OID 33012)
+-- TOC entry 2225 (class 2606 OID 33012)
 -- Name: usuario usuario_fk_dp; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -600,7 +700,7 @@ ALTER TABLE ONLY public.usuario
     ADD CONSTRAINT usuario_fk_dp FOREIGN KEY (departamento_id) REFERENCES public.departamento(id);
 
 
--- Completed on 2020-11-28 22:56:06 -04
+-- Completed on 2020-12-07 17:29:44 -04
 
 --
 -- PostgreSQL database dump complete
